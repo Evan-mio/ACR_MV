cc345b88b82b1cd0e0d748e367ec0e44bf4733f8
Evan-mio <makar199503@gmail.com>
2026-09-12 15:53:29 +0300

    Update users.js

--- DataBaza/baza/users.js
+++ DataBaza/baza/users.js
@@ -3,5 +3,5 @@ const mockUserBase = [
     { "id": 10010020, "password": "Bkmz!10257606", "firstName": "Жамиля", "lastName": "Олейникова", "position": "SysAdmin", "plot": "Office", "company": "OOO Mars", "shift": "Brown"},
     { "id": 10010030, "password": "Mars1234", "firstName": "Анна", "lastName": "Филимонова", "position": "User", "plot": "Laboratory", "company": "OOO Mars", "shift": "Brown"},
     { "id": 10010040,  "password": "Mars123456", "firstName": "Наталья", "lastName": "Верещагина", "position": "Admin", "plot": "Office", "company": "OOO Mars", "shift": ""},
-    { "id": 10010050, "password": "Twix!10257600", "lastName": "Иван",	"lastName": "Макаров", "position":	"User",  "plot": "Production",	"company": "Gradus", "shift": "White"}
+    { "id": 10010050, "password": "Twix!10257600", "firstName": "Иван",	"lastName": "Макаров", "position":	"User",  "plot": "Production",	"company": "Gradus", "shift": "White"}
 ];
