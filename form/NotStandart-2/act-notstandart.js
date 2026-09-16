6ef9b057f0c29dd6ce80d6852602e0ef1a53a829
Evan-mio <makar199503@gmail.com>
2026-09-16 12:31:22 +0300

    Update act-notstandart.js

--- form/NotStandart-2/act-notstandart.js
+++ form/NotStandart-2/act-notstandart.js
@@ -7,7 +7,7 @@ if (typeof DRAFT_DATA_KEY === 'undefined') { var DRAFT_DATA_KEY = 'qa_all_drafts
 if (typeof SHADOW_PREFIX === 'undefined') { var SHADOW_PREFIX = 'shadow_arch_'; }
 if (typeof FINAL_ARCHIVE_PREFIX === 'undefined') { var FINAL_ARCHIVE_PREFIX = 'qaArchive_'; }
 
-var BLANK_VERSION = '4.1.0'; 
+var BLANK_VERSION = '4.2.0'; 
 var mainForm = null;
 
 // Переменные Excel-пакета
@@ -466,7 +466,6 @@ tabs.forEach(tab => {
 
         tab.classList.add('active');
         
-        // Исправленная строка (убрана лишняя скобка в конце):
         const targetContent = document.querySelector(`[data-tab-content="${tab.dataset.tab}"]`);
         targetContent.classList.add('active');
     });
